d4e89a6f2c4f904f71c5e3aa6deed218dc1224aa
Dev-Mau17 <maulidshafii12@gmail.com>
2025-03-29 17:18:19 +0300

    Add files via upload

---
+++ api.php
@@ -0,0 +1,49 @@
+<?php
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: Content-Type");
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "student";
+
+// Connect to MySQL
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die(json_encode(["error" => "Database connection failed"]));
+}
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ($method === 'GET') {
+    // Fetch all students
+    $sql = "SELECT * FROM students";
+    $result = $conn->query($sql);
+    $students = [];
+
+    while ($row = $result->fetch_assoc()) {
+        $students[] = $row;
+    }
+
+    echo json_encode($students);
+}
+
+if ($method === 'GET') {
+    // Fetch all s
+    $sql = "SELECT * FROM subject";
+    $result = $conn->query($sql);
+    $subject = [];
+
+    while ($row = $result->fetch_assoc()) {
+        $subject[] = $row;
+    }
+
+    echo json_encode($subject);
+}
+
+
+$conn->close();
+?>
